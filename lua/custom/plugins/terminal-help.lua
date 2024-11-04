94c6f82f76811ad13882d76107c73c290508fcf2
AK47are <3318517070@qq.com>
2024-11-04 22:15:21 +0800

    修复 terminal-help.lua ，优化其执行 C++ 可执行文件代码。

--- lua/custom/plugins/terminal-help.lua
+++ lua/custom/plugins/terminal-help.lua
@@ -1,24 +1,23 @@
--- vim.g.terminal_shell = 'powershell.exe'
--- 定义一个检查文件是否存在的函数
 vim.keymap.set('n', '<leader>ra', function()
-  print 'OKKKKK\n'
-  -- local files
-  -- if vim.fn.has 'win32' == 1 then
-  --   files = { 'build/%.exe', '%.exe', 'a.exe' }
-  -- else
-  --   files = { 'build/%.out', '%.out', 'a.out' }
-  -- end
-  -- local is_find = false
-  -- for i = 1, #files do
-  --   if vim.fn.filereadable(files[i]) == 1 then
-  --     vim.cmd(':H ' .. files[i])
-  --     is_find = true
-  --     break
-  --   end
-  -- end
-  -- if not is_find then
-  --   print '没有找到可执行文件！\n'
-  -- end
+  local file_name = vim.fn.expand '%:t:r'
+  local files
+  if vim.fn.has 'win32' == 1 then
+    files = { 'build/' .. file_name .. '.exe', file_name .. '.exe', 'a.exe' }
+  else
+    files = { 'build/' .. file_name .. '.out', file_name .. '.out', 'a.out' }
+  end
+  local is_find = false
+  print(files[1], files[2], files[3])
+  for i = 1, #files do
+    if vim.fn.filereadable(files[i]) == 1 then
+      vim.cmd(':H ' .. files[i])
+      is_find = true
+      break
+    end
+  end
+  if not is_find then
+    -- print '没有找到可执行文件！'
+  end
 end, { desc = 'Run' })
 
 return {
